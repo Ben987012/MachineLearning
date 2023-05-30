aa5397a4d8b8eb22919e5f0919c68ed995545a16
Ben987012 <135013835+Ben987012@users.noreply.github.com>
2023-05-30 11:40:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,37 @@ from strokePred import x_test, y_test
 from sklearn.metrics import confusion_matrix, classification_report
 from mlxtend.plotting import plot_confusion_matrix
 
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
 def main():
     st.title('Stroke előrejelző app')
 
     if st.button('Konfúziós mátrix megjelenítése'):
-        cm = confusion_matrix(y_test, knn.predict(x_test))
-        # Megjelenítés
-        fig, ax = plot_confusion_matrix(conf_mat=cm)
+        # Valószínűségek és címkék generálása
+        np.random.seed(0)
+        n_samples = 1000
+        y_true = np.random.randint(2, size=n_samples)
+        y_scores = np.random.rand(n_samples)
+
+        # ROC görbe kiszámítása
+        fpr, tpr, _ = roc_curve(y_true, y_scores)
+        roc_auc = auc(fpr, tpr)
+
+        # Streamlit alkalmazás létrehozása
+        st.title("ROC görbe")
+        st.write("Ez egy egyszerű példa a ROC görbe megjelenítésére a Streamlit segítségével.")
+
+        # ROC görbe kirajzolása
+        fig, ax = plt.subplots()
+        ax.plot(fpr, tpr, label=f'ROC görbe (AUC = {roc_auc:.2f})')
+        ax.plot([0, 1], [0, 1], 'k--')
+        ax.set_xlabel('Hamis pozitív arány')
+        ax.set_ylabel('Valós pozitív arány')
+        ax.set_title('Receiver Operating Characteristic')
+        ax.legend(loc="lower right")
         st.pyplot(fig)
 
     if st.button('Modellek összevetése'):
